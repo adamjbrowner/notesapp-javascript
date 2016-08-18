8ab0160679933519a390ba1168ef88dd064b787c
Adam <adamjbrowner@gmail.com>
2016-08-18 15:00:20 +0100

    Challenge 10

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var list = new noteList();
 var controller = new noteController(list);
 controller._list.addNote("My favourite drink: seltzer");
 controller._list.addNote("My favourite food: burger");
+controller._list.addNote("My favourite film: 12 Angry Men")
 controller.setUpNoteListView();
 controller.HTMLToApp();
